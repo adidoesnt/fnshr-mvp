7867827911f1713379a2ea639bb24db7db35fb74
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-05-25 23:13:36 +0800

    make own tasks collapsible

--- src/components/YourTasks.tsx
+++ src/components/YourTasks.tsx
@@ -30,9 +30,74 @@ export const convertISOToLocalTime = (deadline: string) => {
   return `${dateString} ${timeString}`;
 };
 
-export default function YourTasks({ username }: YourTasksProps) {
-  const dispatch = useDispatch();
+type TaskCardProps = {
+  id: string;
+  prompts: string[];
+  status: "ongoing" | "completed" | "missed";
+  name: string;
+  pledge: number;
+  convertedDeadline: string;
+};
+
+function TaskCard({
+  id,
+  prompts,
+  status,
+  name,
+  pledge,
+  convertedDeadline,
+}: TaskCardProps) {
+  const [expand, setExpand] = useState(false);
   const URI = "api/completeTask";
+  const dispatch = useDispatch();
+  async function handleCompleteTask(id: string) {
+    try {
+      const response = await axios.post(URI, { id });
+      const { points } = response.data;
+      dispatch(setPoints(points));
+      console.log(response.data);
+      store.dispatch(fetchTasks());
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <Card key={id} m={2.5}>
+      <TaskPrompts prompts={prompts} status={status} />
+      <CardHeader>
+        <Heading fontSize={20}>Task Name:</Heading>
+        <Text>{name}</Text>
+      </CardHeader>
+      {!expand ? <Button onClick={() => setExpand(true)}>Expand</Button> : null}
+      {expand ? (
+        <>
+          <CardBody>
+            <Heading fontSize={20}>Pledge Amount</Heading>
+            <Text>{pledge} points</Text>
+            <br />
+            <Heading fontSize={20}>Deadline</Heading>
+            <Text>{convertedDeadline}</Text>
+          </CardBody>
+          <CardFooter display={"flex"} flexDir={"column"}>
+            <Heading fontSize={20}>Task Status:</Heading>
+            <Text>{status}</Text>
+            {status === "ongoing" ? (
+              <Button m={2.5} onClick={() => handleCompleteTask(id)}>
+                Mark Complete
+              </Button>
+            ) : null}
+          </CardFooter>
+          {expand ? (
+            <Button onClick={() => setExpand(false)}>Collapse</Button>
+          ) : null}
+        </>
+      ) : null}
+    </Card>
+  );
+}
+
+export default function YourTasks({ username }: YourTasksProps) {
   const tasks = useSelector(selectAllTasks);
   const [buttonText, setButtonText] = useState("Show");
   const [filter, setFilter] = useState("All");
@@ -62,18 +127,6 @@ export default function YourTasks({ username }: YourTasksProps) {
     }
   };
 
-  async function handleCompleteTask(id: string) {
-    try {
-      const response = await axios.post(URI, { id });
-      const { points } = response.data;
-      dispatch(setPoints(points));
-      console.log(response.data);
-      store.dispatch(fetchTasks());
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
   return (
     <Card display={"flex"} flexDir={"column"} w={"90%"} m={50} mb={2.5}>
       <Center alignItems={"center"}>
@@ -81,7 +134,9 @@ export default function YourTasks({ username }: YourTasksProps) {
           Your Tasks
         </Heading>
         <Flex flexDir={"column"} m={2.5} w={"50%"}>
-          <Button mb={2.5} onClick={toggleButtonText}>{buttonText}</Button>
+          <Button mb={2.5} onClick={toggleButtonText}>
+            {buttonText}
+          </Button>
           <TaskFilterMenu setFilter={setFilter} />
         </Flex>
       </Center>
@@ -91,29 +146,15 @@ export default function YourTasks({ username }: YourTasksProps) {
             const convertedDeadline = convertISOToLocalTime(deadline);
 
             return (
-              <Card key={index} m={2.5}>
-                <TaskPrompts prompts={prompts} status={status} />
-                <CardHeader>
-                  <Heading fontSize={20}>Task Name:</Heading>
-                  <Text>{name}</Text>
-                </CardHeader>
-                <CardBody>
-                  <Heading fontSize={20}>Pledge Amount</Heading>
-                  <Text>{pledge} points</Text>
-                  <br />
-                  <Heading fontSize={20}>Deadline</Heading>
-                  <Text>{convertedDeadline}</Text>
-                </CardBody>
-                <CardFooter display={"flex"} flexDir={"column"}>
-                  <Heading fontSize={20}>Task Status:</Heading>
-                  <Text>{status}</Text>
-                  {status === "ongoing" ? (
-                    <Button m={2.5} onClick={() => handleCompleteTask(id)}>
-                      Mark Complete
-                    </Button>
-                  ) : null}
-                </CardFooter>
-              </Card>
+              <TaskCard
+                key={id}
+                convertedDeadline={convertedDeadline}
+                id={id}
+                name={name}
+                pledge={pledge}
+                status={status}
+                prompts={prompts}
+              />
             );
           })
         : null}
