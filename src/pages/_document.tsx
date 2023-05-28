c8ea2c75c5b6ec669e03bdc5ef22d6a6189dfc9b
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-05-29 03:02:13 +0800

    add manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 require("dotenv").config();
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
