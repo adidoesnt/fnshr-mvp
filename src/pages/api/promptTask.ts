bed3b42a61c651b0ada23ddd1fc4fa65238c0e0f
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-06-02 13:14:11 +0800

    notify upon task prompt

--- src/pages/api/promptTask.ts
+++ src/pages/api/promptTask.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { initDb, closeDb } from "./repository";
 import { Task } from "./schemas";
+import axios from "axios";
 
 type UpdateStatus = "success" | "failure";
 
@@ -9,6 +10,22 @@ type Data = {
   prompts?: string[];
   status: UpdateStatus;
 };
+
+const API_PREFIX =
+  process.env.ENV === "PROD"
+    ? process.env.CLOUD_API_PREFIX
+    : process.env.LOCAL_API_PREFIX;
+
+async function notifyFriend(username: string, name: string, friend: string) {
+  const URI = `${API_PREFIX}notifyFriend`;
+  const content = `${username} has called you out for missing your task: "${name}"`;
+  try {
+    const response = await axios.post(URI, { friend, content });
+    console.log(response.data);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -19,10 +36,11 @@ export default async function handler(
     const { id, prompter } = req.body;
     try {
       const task = await Task.findOne({ _id: id });
-      const { prompts } = task;
+      const { name, prompts, username } = task;
       prompts.push(prompter);
       await Task.updateOne({ _id: id }, { prompts });
       await closeDb();
+      await notifyFriend(prompter, name, username);
       res.status(200).json({ id, prompts, status: "success" });
     } catch {
       await closeDb();
