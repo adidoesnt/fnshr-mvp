548d52711bcdba0ea1fbcad0d749bb5f4ab57f51
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-06-05 17:15:31 +0800

    fix firebase refresh error

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -3,17 +3,27 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig ={
-    apiKey: process.env.FB_APIKEY,
-    authDomain: process.env.FB_AUTHDOMAIN,
-    projectId: process.env.FB_PROJID,
-    storageBucket: process.env.FB_STORAGEBUCKET,
-    messagingSenderId: process.env.FB_MSGID,
-    appId: process.env.FB_APPID,
-    measurementId: process.env.FB_MESID,
+const firebaseConfig = {
+  apiKey: process.env.FB_APIKEY,
+  authDomain: process.env.FB_AUTHDOMAIN,
+  projectId: process.env.FB_PROJID,
+  storageBucket: process.env.FB_STORAGEBUCKET,
+  messagingSenderId: process.env.FB_MSGID,
+  appId: process.env.FB_APPID,
+  measurementId: process.env.FB_MESID,
+};
+
+let app;
+export let analytics: any;
+export let storage: any;
+
+if (firebaseConfig?.projectId) {
+  app = initializeApp(firebaseConfig);
+  if (app.name && typeof window !== "undefined") {
+    analytics = getAnalytics(app);
   }
 
-const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-export const storage = getStorage(app);
+  // Access Firebase services using shorthand notation
+  storage = getStorage();
+}
 export default app;
